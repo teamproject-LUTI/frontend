780a8871e0b68b011f2cbca5cf6d8cca0bce6c63
jjong <whdgh9588@gmail.com>
2025-06-12 14:53:50 +0900

    DOCS: #8 인증 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
-import Login from "./pages/login/Login"
+import Login from "./pages/login/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import OAuth2ErrorPage from "./pages/login/OAuth2ErrorPage";
 
 const App = () => {
   return (
       <div id='app'>
         <Routes>
-          <Route exact path='/' element={<Login/>}/>
-          <Route exact path='/main' element={<Main/>}/>
+          {/*로그인 페이지 - 보호 안함 */}
+          <Route path='/' element={<Login />} />
+          <Route path='/login' element={<Login />} />
+
+          {/* OAuth2 관련 페이지 */}
+          <Route path='/auth/error' element={<OAuth2ErrorPage />} />
+
+          {/*메인 페이지*/}
+          <Route path='/main' element={
+            <ProtectedRoute>
+              <Main />
+            </ProtectedRoute>
+          } />
         </Routes>
       </div>
   );
