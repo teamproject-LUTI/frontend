e5576d370f9280d6157ea21e02c0e4ec90992fb5
김원기 <hina361@naver.com>
2025-06-12 15:40:42 +0900

    [FEAT] PaymentButtonCom 생성

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Login from "./pages/login/Login"
+import PaymentTestPage from "./routes/PaymentTestPage";
 
 const App = () => {
   return (
@@ -9,7 +10,9 @@ const App = () => {
         <Routes>
           <Route exact path='/' element={<Login/>}/>
           <Route exact path='/main' element={<Main/>}/>
+            <Route path="/test-payment" element={<PaymentTestPage />} />
         </Routes>
+
       </div>
   );
 }
