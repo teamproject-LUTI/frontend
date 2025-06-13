a654718ffc087b4d6a4d428cd83888aed76d7d17
jjong <whdgh9588@gmail.com>
2025-06-13 09:19:01 +0900

    FEAT: #15  구현된 화면 라우터 설정

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,42 @@ import Login from "./pages/login/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 import OAuth2ErrorPage from "./pages/login/OAuth2ErrorPage";
 import Membership from "./pages/login/Membership";
+import MyPage from "./pages/mypage/MyPage"
+import MyPageProfile from "./pages/mypage/MyPageProfile"
+import { AuthProvider } from "./util/AuthContext";
 
 const App = () => {
   return (
-      <div id='app'>
-        <Routes>
-          {/*로그인 페이지 - 보호 안함 */}
-          <Route path='/' element={<Login />} />
-          <Route path='/login' element={<Login />} />
+      <AuthProvider>
+        <div id='app'>
+          <Routes>
+            {/*로그인 페이지 - 보호 안함 */}
+            <Route path='/' element={<Login />} />
+            <Route path='/login' element={<Login />} />
 
-          {/* OAuth2 관련 페이지 */}
-          <Route path='/auth/error' element={<OAuth2ErrorPage />} />
+            {/* OAuth2 관련 페이지 */}
+            <Route path='/auth/error' element={<OAuth2ErrorPage />} />
 
-          {/*메인 페이지*/}
-          <Route path='/main' element={
-            <ProtectedRoute>
-              <Main />
-            </ProtectedRoute>
-          } />
-          {/*멤버쉽 페이지*/}
-          <Route path='/membership' element={
-            <ProtectedRoute>
-              <Membership />
-            </ProtectedRoute>
-          } />
-        </Routes>
-      </div>
+            {/*메인 페이지*/}
+            <Route path='/main' element={
+              <ProtectedRoute>
+                <Main />
+              </ProtectedRoute>
+            } />
+            {/*멤버쉽 페이지*/}
+            <Route path='/membership' element={
+              <ProtectedRoute>
+                <Membership />
+              </ProtectedRoute>
+            } />
+            {/* 마이페이지*/}
+            <Route path='/mypage' element={<ProtectedRoute />}>
+              <Route index element={<MyPage />} />
+              <Route path='profile' element={<MyPageProfile />} />
+            </Route>
+          </Routes>
+        </div>
+      </AuthProvider>
   );
 }
 
